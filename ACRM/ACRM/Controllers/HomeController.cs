4878814c131f2917001c52e7a17fd2c2a96a2158
pootzko <pootzko@gmail.com>
2013-12-15 10:00:56 +0100

    Initial commit.

---
+++ ACRM/ACRM/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACRM.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
