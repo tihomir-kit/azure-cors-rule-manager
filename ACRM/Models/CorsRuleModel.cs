1c0f6b48dca316d25923941f44e727c884f4d4a3
pootzko <pootzko@gmail.com>
2013-12-15 16:53:05 +0100

    (Re)Moved ACRM parent folder.

---
+++ ACRM/Models/CorsRuleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACRM.Models
+{
+    public class CorsRuleModel
+    {
+        public int Id { get; set; }
+        public IList<string> AllowedOrigins { get; set; }
+        public IList<string> AllowedMethods { get; set; }
+        public IList<string> AllowedHeaders { get; set; }
+        public IList<string> ExposedHeaders { get; set; }
+        public int MaxAgeInSeconds { get; set; }
+    }
+}
---
+++ ACRM/Models/CorsRulesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACRM.Models
+{
+    public class CorsRulesModel
+    {
+        public IList<CorsRuleModel> CorsRules { get; set; }
+    }
+}
